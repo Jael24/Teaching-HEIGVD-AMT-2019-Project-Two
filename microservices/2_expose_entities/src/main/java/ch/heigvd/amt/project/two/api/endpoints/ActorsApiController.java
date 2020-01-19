c2094542cc6d944ec9dbe55aff04ff975956d0b0
Guillaume Vetter <guillaume.vetter@heig-vd.ch>
2020-01-19 22:57:28 +0100

    Test of pagination and cucumber

--- microservices/2_expose_entities/src/main/java/ch/heigvd/amt/project/two/api/endpoints/ActorsApiController.java
+++ microservices/2_expose_entities/src/main/java/ch/heigvd/amt/project/two/api/endpoints/ActorsApiController.java
@@ -1,6 +1,5 @@
 package ch.heigvd.amt.project.two.api.endpoints;
 
-import ch.heigvd.amt.project.two.api.ActorsApi;
 import ch.heigvd.amt.project.two.api.exceptions.NotFoundException;
 import ch.heigvd.amt.project.two.configuration.JwtTokenUtil;
 import ch.heigvd.amt.project.two.entities.ActorEntity;
@@ -8,26 +7,34 @@ import ch.heigvd.amt.project.two.model.Actor;
 import ch.heigvd.amt.project.two.repositories.ActorRepository;
 import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.StringJoiner;
+import java.util.stream.Collectors;
 
 @Controller
-public class ActorsApiController implements ActorsApi {
+public class ActorsApiController {
 
     @Autowired
     ActorRepository actorRepository;
+
+    @Autowired
+    HttpServletRequest httpServletRequest;
 
     public ResponseEntity<Object> createActor(@ApiParam(value = "" ,required=true) @RequestHeader(value="Authorization", required=true) String authorization,@ApiParam(value = "" ,required=true )  @Valid @RequestBody Actor actor) {
         ActorEntity newActorEntity = toActorEntity(actor);
@@ -75,6 +82,37 @@ public class ActorsApiController implements ActorsApi {
         }
     }
 
+    public ResponseEntity<List<Actor>> getActorByUser(@Valid Integer page, @Valid Integer pageSize) throws Exception {
+        int userId = (Integer) httpServletRequest.getAttribute("user_id");
+
+        long totalActorCount = actorRepository.countByUserId(userId);
+        long lastPageNumber = totalMatchCount / pageSize + ((totalMatchCount % pageSize == 0) ? 0 : 1);
+
+        StringJoiner linkHeader = new StringJoiner(", ");
+
+        String str = constructPageUri(httpServletRequest.getRequestURI(), "next", page + 1, pageSize, lastPageNumber);
+        if (str != "") {
+            linkHeader.add(str);
+        }
+        if (page != lastPageNumber) {
+            linkHeader.add(constructPageUri(httpServletRequest.getRequestURI(), "last", lastPageNumber, pageSize, lastPageNumber));
+        }
+        if (page != 1) {
+            linkHeader.add(constructPageUri(httpServletRequest.getRequestURI(), "first", 1, pageSize, lastPageNumber));
+        }
+
+        str = constructPageUri(httpServletRequest.getRequestURI(), "prev", page - 1, pageSize, lastPageNumber);
+        if (str != "") {
+            linkHeader.add(str);
+        }
+
+        List<Actor> matches = actorRepository.findByUserId((Integer) httpServletRequest.getAttribute("user_id"), PageRequest.of(page-1, pageSize)).parallelStream()
+                .map(ActorsApiController::toActor).collect(Collectors.toList());
+        return ResponseEntity.ok().header(HttpHeaders.LINK, linkHeader.toString()).body(matches);
+    }
+
+
+
     public ResponseEntity<List<Actor>> getActor(@ApiParam(value = "" ,required=true) @RequestHeader(value="Authorization", required=true) String authorization,@ApiParam(value = "The actor's id",required=true) @PathVariable("id") Long id) {
         ActorEntity actorEntity = actorRepository.findById(id).orElse(null);
 
@@ -97,7 +135,7 @@ public class ActorsApiController implements ActorsApi {
         return emailOwnerFromToken.equals(actor.getOwner());
     }
 
-    private ActorEntity toActorEntity(Actor actor) {
+    private static ActorEntity toActorEntity(Actor actor) {
         ActorEntity entity = new ActorEntity();
         entity.setEmail(actor.getEmail());
         entity.setFullname(actor.getFullname());
@@ -105,7 +143,7 @@ public class ActorsApiController implements ActorsApi {
         return entity;
     }
 
-    private Actor toActor(ActorEntity entity) {
+    private static Actor toActor(ActorEntity entity) {
         Actor actor = new Actor();
         entity.setEmail(actor.getEmail());
         entity.setFullname(actor.getFullname());
--- microservices/2_expose_entities/src/main/java/ch/heigvd/amt/project/two/repositories/ActorRepository.java
+++ microservices/2_expose_entities/src/main/java/ch/heigvd/amt/project/two/repositories/ActorRepository.java
@@ -1,8 +1,21 @@
 package ch.heigvd.amt.project.two.repositories;
 
 import ch.heigvd.amt.project.two.entities.ActorEntity;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
 
 
 public interface ActorRepository extends CrudRepository<ActorEntity, Long>{
+
+    List<ActorEntity> findAllById(int id);
+
+    // used to get data in a pageable fashion
+    @Cacheable("actorListPaged")
+    List<ActorEntity> findById(int id, Pageable pageable);
+
+    @Cacheable("actorCount")
+    long countById(int userId);
 }
